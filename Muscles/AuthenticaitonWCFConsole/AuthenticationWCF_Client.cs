7deb7f87f1420e50fb6da97986140340236dd1e7
Matthew Buller <mbuller@regis.edu>
2013-12-07 22:10:29 -0600

    WCF

---
+++ Muscles/AuthenticaitonWCFConsole/AuthenticationWCF_Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuthenticaitonWCFConsole.ServiceReference1;
+
+namespace AuthenticaitonWCFConsole
+{
+    class AuthenticationWCF_Client
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceReference1.LoginWCFImplClient proxy = new LoginWCFImplClient()
+;
+            while (true)
+            {
+                //String s = proxy.Hello("JohnUN", "DoePW");
+                Console.Out.WriteLine("Enter user name");
+
+                String un = Console.ReadLine();
+                Console.Out.WriteLine("Enter pw");
+
+                String pw = Console.ReadLine();
+                Boolean b = proxy.Login(un, pw);
+                Console.Out.WriteLine(b);
+                //Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Muscles/MusclesAuthenticatoinWCF_Host/WCF_Host.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusclesAuthenticationWCF;
+using System.ServiceModel;
+
+namespace MusclesAuthenticatoinWCF_Host
+{
+    class WCF_Host
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Out.WriteLine("Starting Host");
+                ServiceHost host = new ServiceHost(typeof(MusclesAuthenticationWCF.AuthenticateWCFImpl));
+                host.Open();
+                Console.Out.WriteLine("Service started; enter Return to exit application");
+                Console.ReadLine();
+                host.Close();
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
